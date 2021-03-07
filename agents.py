588bcadd816a42eebdbaad1e15ddd624b0375732
ayansengupta998 <ayansengupta@umass.edu>
2021-03-07 16:03:26 -0500

    fix bugs

--- agents.py
+++ agents.py
@@ -50,66 +50,14 @@ class MinimaxAgent:
                 best_util, best_move, best_state = util, move, state
         return best_move, best_state
 
-    def bubble_up(self, parents, popped, state):
-        # print("Test", state, type(state))
-        utility = popped[0].utility()
-        print("Utility", utility)
-        minimax = popped[1]
-        print(minimax,"mini")
-        current_child = popped[0]
-        # print("current_child", current_child, type(current_child))
-        parents[current_child] = (parents[current_child][0],utility)
-        # print("Testing Testing", type(parents[current_child][0]))
-        current_parent= parents[popped[0]][0]
-        # print("Parent", current_parent[0], type(current_parent[0])
-        while(current_child != state):
-            if minimax == 1:
-                #update parent with min of the values
-                if parents[current_parent][1] == 0 or parents[current_parent][1] == 123456789:
-                    parents[current_parent] = (parents[current_parent][0], utility)
-                else:
-                    parents[current_parent] = (parents[current_parent][0], min(parents[current_parent][1],utility))
-            else:
-                #update parent with max of the values
-                if parents[current_parent][1] == 0 or parents[current_parent][1] == 123456789:
-                    parents[current_parent] = (parents[current_parent][0], utility)
-                else:
-                    parents[current_parent] = (parents[current_parent][0], max(parents[current_parent][1],utility))
-            
-            utility = parents[current_parent][1]
-            minimax =  minimax*-1
-            current_child = current_parent
-            current_parent = parents[current_child][0]
-
-        return utility
 #issue facing is that the states in the dicitonary seem to be behaving erratically. can't seem to find the key stored even though it is in the dict
 #mismatch of object types
 # The utility function should ideally be returning a non zero value
 
     def minimax(self, state):
-        # print("TESTER", state, type(state))
-        stack = []
-        visited = dict()
-        parents = dict()
-        parents[state] = ('',123456789)
-        stack.append((state, 1))    
-        utility = 0
-        while(len(stack) != 0):
-            popped = stack.pop(len(stack)-1)
-            # print("Popped:", popped[0], type(popped[0]))
-            visited[popped[0]] = ""
-            if(popped[0].is_full()==True):
-                print(popped[0].is_full(),"TESTING")
-                utility = self.bubble_up(parents, popped, state)
-            else:
-                successors = popped[0].successors()
-                for succ in successors:
-                    # print("BIGGER TEST:", succ, succ[1], type(succ[1]))
-                    if succ[1] not in visited:
-                        stack.append((succ[1], popped[1]*-1))
-                        parents[succ[1]] = (popped[0], 0)
-        print("Utility:", utility)
-        return utility
+        print("in here")
+        
+        return state.utility()
 
 
 class MinimaxHeuristicAgent(MinimaxAgent):
